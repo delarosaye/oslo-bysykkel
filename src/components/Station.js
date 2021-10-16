5be209571ce975308b59eb784aa13ec7b7da4740
delarosaye <72583102+delarosaye@users.noreply.github.com>
2021-10-16 15:02:55 +0200

    Update Station.js

--- src/components/Station.js
+++ src/components/Station.js
@@ -5,10 +5,10 @@ import {
     StationContent, StationContentValue
 } from '../Styles/StattionElements'
 
-const Station = ({station, id}) => {
+const Station = ({station, counter, size, id}) => {
     return (
         <StationContainer>
-            <StationName>{station?.name}</StationName>
+            <StationName>{counter===0 ? (id + 1) : (size * counter) + id + 1}. {station?.name}</StationName>
             <StationContent>Antall låser: <StationContentValue>{station?.capacity}</StationContentValue></StationContent>
             <StationContent>Antall tilgjenglige låser: <StationContentValue>{station?.availabilities?.num_bikes_available}</StationContentValue></StationContent>
             <StationContent>Adresse: <StationContentValue>{station?.address}</StationContentValue></StationContent>
